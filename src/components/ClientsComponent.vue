bd1b29b8bbee1bd610b5076336d9ed8192b62661
ValMtp <115800717+ValMtp3@users.noreply.github.com>
2024-12-29 15:22:10 +0100

    Add lazy loading for images and HTML/JSX cleanup
    Added the `loading="lazy"` attribute to optimize image loading across multiple Vue components. Additionally, cleaned up HTML/JavaScript formatting by removing unnecessary spaces and improving consistency in code structure for better readability.

--- src/components/ClientsComponent.vue
+++ src/components/ClientsComponent.vue
@@ -1,35 +1,33 @@
-<script setup >
+<script setup>
 defineProps(
-    {
-      alt: {
-        type: String,
-        required: true
-      },
-      image: {
-        type: String,
-        required: true
-      },
-      name: {
-        type: String,
-        required: true
-      },
-      role: {
-        type: String,
-        required: true
-      },
-    }
+  {
+    alt: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      required: true
+    },
+  }
 )
-</script >
+</script>
 
-<template >
-  <div class="flex flex-col items-center" >
-    <img
-        :alt="alt" :src="image"
-        :title="name"
-        class="w-12 h-12 lg:w-20 lg:h-20 xl:w-40 xl:h-40 rounded-3xl " >
-    <div class="p-4 text-center" >
-      <p class="mt-1 md:text-md font-bold xl:text-xl text-neutral-900 flex justify-center text-start" >{{ name }}</p >
-      <p class="md:text-sm xl:text-base mt-1 text-neutral-700 flex justify-center text-start" >{{ role }}</p >
-    </div >
-  </div >
-</template >
+<template>
+  <div class="flex flex-col items-center">
+    <img :alt="alt" :src="image" :title="name" class="w-12 h-12 lg:w-20 lg:h-20 xl:w-40 xl:h-40 rounded-3xl "
+      loading="lazy">
+    <div class="p-4 text-center">
+      <p class="mt-1 md:text-md font-bold xl:text-xl text-neutral-900 flex justify-center text-start">{{ name }}</p>
+      <p class="md:text-sm xl:text-base mt-1 text-neutral-700 flex justify-center text-start">{{ role }}</p>
+    </div>
+  </div>
+</template>
