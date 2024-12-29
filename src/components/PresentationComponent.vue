ec5fc2a837ee422062a9f1f28fe3ff18e17145d9
ValMtp <115800717+ValMtp3@users.noreply.github.com>
2024-12-29 17:40:56 +0100

    Clean up HTML structure and optimize CSS and components
    Added consistent spacing in HTML attributes and improved formatting across multiple Vue components for better readability. Removed unused CSS classes to reduce clutter. Introduced an `.htaccess` file for URL rewriting, enhancing navigation and SEO compatibility.

--- src/components/PresentationComponent.vue
+++ src/components/PresentationComponent.vue
@@ -1,20 +1,20 @@
-
 <template >
   <div class="bg-white font-[AnonymousPro] p-1 rounded-3xl w-full max-w-none" >
-    <div class="flex flex-col items-center xl:flex-row xlm:justify-between m-10">
+    <div class="flex flex-col items-center xl:flex-row xlm:justify-between m-10" >
       <div class=" mb-6 sm:mb-0" >
         <img
             alt="photo Valentin Fiess"
             class="rounded-3xl object-cover shadow-xl shadow-black/40 h-72 w-72"
             src="/assets/assets_index/Profile.webp" />
       </div >
-      <div class="text-center">
+      <div class="text-center" >
         <h1 class="text-xl sm:text-3xl font-regular mx-20
 mb-6 font-bold " >Valentin Fiess</h1 >
-        <h2 class=" text-base sm:text-lg underline mx-20 mb-6" >Développeur Web</h2 >
-        <h3 class="max-w-md  px-10">
-          J'ai 22 ans et je suis un développeur débutant à Montpellier et etudiant a l’EPSI, je m'intéresse aux nouvelles technologies, notamment celle du web et de l’IA.
-        </h3>
+        <h2 class=" text-base sm:text-lg underline mx-20 mb-6" >Développeur Web / IA</h2 >
+        <h3 class="max-w-md  px-10" >
+          J'ai 22 ans et je suis un développeur débutant à Montpellier et etudiant a l’EPSI, je m'intéresse aux
+          nouvelles technologies, notamment celle du web et de l’IA.
+        </h3 >
       </div >
     </div >
   </div >
--- src/components/include/Footer.vue
+++ src/components/include/Footer.vue
@@ -1,45 +1,47 @@
-<template>
-  <div class="bg-[#FBFDFF]">
-    <footer class="bg-[#FBFDFF] font-[AnonymousPro]">
+<template >
+  <div class="bg-[#FBFDFF]" >
+    <footer class="bg-[#FBFDFF] font-[AnonymousPro]" >
       <button id="back-to-top-btn" class="fixed bottom-4 right-4 bg-sky-700 text-white rounded-xl p-3
      hover:bg-neutral-800
-     transition duration-300 text-4xl" title="retour en haut">
+     transition duration-300 text-4xl" title="retour en haut" >
         ↑
-      </button>
-      <div class="container mx-auto p-10">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-          <div class="col-span-1">
-            <h1 class="text-3xl text-neutral-900 font-bold">Liens utiles</h1>
-            <ul class="mt-5 grid grid-cols-3 gap-4">
-              <li class="text-lg text-neutral-900 font-light">
-                <router-link to="/" class="hover:underline" @click.native="scrollToTop">Acceuil</router-link>
-              </li>
-              <li class="text-lg text-neutral-900 font-light">
-                <router-link to="/stack" class="hover:underline" @click.native="scrollToTop">Stack</router-link>
-              </li>
-              <li class="text-lg text-neutral-900 font-light">
-                <router-link to="/projets" class="hover:underline" @click.native="scrollToTop">Projets</router-link>
-              </li>
-              <li class="text-lg text-neutral-900 font-light">
-                <router-link to="/legal" class="hover:underline" @click.native="scrollToTop">Mention
-                  légales</router-link>
-              </li>
-              <li class="text-lg text-neutral-900 font-light">
-                <router-link to="/policy" class="hover:underline " @click.native="scrollToTop">Politique
-                  confidentialité</router-link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </footer>
-  </div>
-  <p class="text-md text-center text-neutral-900 font-light text-2xl m-3">
+      </button >
+      <div class="container mx-auto p-10" >
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10" >
+          <div class="col-span-1" >
+            <p class="text-3xl text-neutral-900 font-bold" >Liens utiles</p >
+            <ul class="mt-5 grid grid-cols-3 gap-4" >
+              <li class="text-lg text-neutral-900 font-light" >
+                <router-link class="hover:underline" to="/" @click.native="scrollToTop" >Acceuil</router-link >
+              </li >
+              <li class="text-lg text-neutral-900 font-light" >
+                <router-link class="hover:underline" to="/stack" @click.native="scrollToTop" >Stack</router-link >
+              </li >
+              <li class="text-lg text-neutral-900 font-light" >
+                <router-link class="hover:underline" to="/projets" @click.native="scrollToTop" >Projets</router-link >
+              </li >
+              <li class="text-lg text-neutral-900 font-light" >
+                <router-link class="hover:underline" to="/legal" @click.native="scrollToTop" >Mention
+                                                                                              légales
+                </router-link >
+              </li >
+              <li class="text-lg text-neutral-900 font-light" >
+                <router-link class="hover:underline " to="/policy" @click.native="scrollToTop" >Politique
+                                                                                                confidentialité
+                </router-link >
+              </li >
+            </ul >
+          </div >
+        </div >
+      </div >
+    </footer >
+  </div >
+  <p class="text-md text-center text-neutral-900 font-light text-2xl m-3" >
     Développé en VueJS par Valentin Fiess ❤️
-  </p>
-</template>
-<script lang="ts" setup>
+  </p >
+</template >
+<script lang="ts" setup >
 const scrollToTop = () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+  window.scrollTo({top: 0, behavior: 'smooth'});
 };
-</script>
+</script >
